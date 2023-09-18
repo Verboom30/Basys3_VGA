4e3d091b5e0d1e67e9607631d30257fcbf3e4ea6
Clement <56961627+Verboom30@users.noreply.github.com>
2023-09-18 21:33:41 +0200

    Update Clock_manager

--- SRC/Clock/Clock_manager.vhd
+++ SRC/Clock/Clock_manager.vhd
@@ -11,86 +11,67 @@ use IEEE.NUMERIC_STD.ALL;
 --use UNISIM.VComponents.all;
 
 entity Clock_manager is
-    Port ( H : in STD_LOGIC;
-           RAZ : in STD_LOGIC;
-           CEaff : out STD_LOGIC;
-		   CEseg : out STD_LOGIC;
-           CEincrement : out STD_LOGIC;
-           CETraitement : out STD_LOGIC);
+  Port (  
+          CLK 	    : in  STD_LOGIC ;
+          RESET     : in  STD_LOGIC ;
+          CLK_50M   : out STD_LOGIC ;
+          CLK_25M   : out STD_LOGIC ;
+          CLK_3K    : out STD_LOGIC ;
+          CLK_1H    : out STD_LOGIC
+        );
 end Clock_manager;
 
-architecture Behavioral of Clock_manager is
-	signal clk25M 	 :std_logic :='0';
-	signal Cpt3k 	: unsigned (15 downto 0);
-	signal Cpt300 	: unsigned (18 downto 0);
-	signal Cpt1 	: unsigned (26 downto 0);
-	signal clk50M : std_logic :='0';
+architecture rtl of Clock_manager is
+	signal counter_3K : unsigned (14 downto 0);
+  signal counter_1H : unsigned (23 downto 0);
 begin
 
-	clk_clk50M:process(H)
+	proc_50M:process(RESET, CLK)
     begin
-        if(H'event and H ='1')then
-            clk50M <= not clk50M;
-        end if;
-    end process;
+      if(RESET ='1') then
+        CLK_50M <= '0';
+      elsif (rising_edge(CLK)) then
+        CLK_50M <= not CLK_50M;
+      end if;
+  end process proc_50M;
 
-    clk_clk25M:process(clk50M)
-    begin
-        if(clk50M'event and clk50M ='1')then
-            clk25M <= not clk25M;
-        end if;
-    end process;
-	CETraitement<=clk25M;
-	
-	clock_3kHz : process (H, RAZ)
-	begin
-		if (RAZ = '1') then
-			Cpt3k <= "0000000000000000";
-			CEaff <= '0';
-		elsif rising_edge(H) then
-			if (Cpt3k = 33333) then
-				CEaff <= '1';
-				Cpt3k <= "0000000000000000";
-			else
-				Cpt3k <= Cpt3k + 1;
-				CEaff <= '0';
-			end if;
-		end if;
-	
-	end process;
+  proc_25M:process(RESET, CLK_50M)
+  begin
+    if(RESET ='1') then
+      CLK_25M <= '0';
+    elsif (rising_edge(CLK_50M)) then
+      CLK_25M <= not CLK_25M;
+    end if;
+  end process proc_25M;
 
-	clock_300 : process (H, RAZ)
-	begin
-		if (RAZ = '1') then
-			Cpt300 <= (others => '0');
-			CEseg <= '0';
-		elsif rising_edge(H) then
+  proc_3K:process(RESET, CLK)
+  begin
+    if(RESET ='1') then
+      CLK_3K <= '0';
+      counter_3K <= (others => '0');
+    elsif (rising_edge(CLK)) then
+      if(counter_3K <= 33333-1) then
+        counter_3K  <= counter_3K + 1;
+        CLK_3K      <= not CLK_3K;
+      else
+        counter_3K <= (others => '0');
+      end if;
+    end if;
+  end process proc_3K;
 
-			if (Cpt300 = 333333) then
-				Cpt300 <= (others => '0');
-				CEseg <= '1';
-			else
-			    Cpt300 <= Cpt300 + 1;
-				CEseg <= '0';
-			end if;
-		end if;
-	end process;
-	
-	clock_1Hz : process (H, RAZ)
-	begin
-		if (RAZ = '1') then
-			Cpt1 <= "000000000000000000000000000";
-			CEIncrement <= '0';
-		elsif rising_edge(H) then
-
-			if (Cpt1 = 100000000-1) then
-				Cpt1 <= "000000000000000000000000000";
-				CEIncrement <= '1';
-			else
-				Cpt1 <= Cpt1 + 1;
-				CEIncrement <= '0';
-			end if;
-		end if;
-	end process;
-
-end Behavioral;
+  proc_1H:process(RESET, CLK)
+  begin
+    if(RESET ='1') then
+      CLK_1H <= '0';
+      counter_1H <= (others => '0');
+    elsif (rising_edge(CLK)) then
+      if(counter_1H <= 25000000-1) then
+        counter_1H  <= counter_1H + 1;
+        CLK_1H      <= not CLK_1H;
+      else
+      counter_1H <= (others => '0');
+      end if;
+    end if;
+  end process proc_1H;
+  
+end rtl;
